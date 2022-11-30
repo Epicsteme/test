a23a8495b7fd0a58a2558269bfa302245d43035b
Epicsteme <Epicsteme@users.noreply.github.com>
2022-11-30 22:12:48 +0530

    added script again

---
+++ acc.js
@@ -0,0 +1,9 @@
+$('.panel-collapse').on('shown.bs.collapse', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var $panel = $(this).closest('.panel');
+    $('html,body').animate({
+        scrollTop: $panel.offset().top
+    }, 500);
+});

d32d481c3953c7c03dd2baca9410f6aa1bdaa375
Epicsteme <Epicsteme@users.noreply.github.com>
2022-12-25 21:00:02 +0530

    copying the stable version

--- main.js
+++
@@ -1,9 +0,0 @@
-('accordionOpened', (el) => {
-    const openedItem = el;
-    const offset = $(openedItem).offset().top;
-    const navHeight = 56;
-    const toolbarHeight = 48;
-    const scrollTop = $(el).parents('.page-content').scrollTop();
-    $(el).parents('.page-content').scrollTop((offset - navHeight - toolbarHeight + scrollTop), 300);
-
-})

392963c8b0287451038cbd2ebf7ad77f51e645f8
Epicsteme <Epicsteme@users.noreply.github.com>
2023-10-06 22:56:18 +0530

    Update common.js

--- common.js
+++ common.js
@@ -5,68 +5,69 @@ class MyHeader extends HTMLElement {
         
         <div class="container-lg d-flex flex-column" id="header">
 
-        <nav class="navbar navbar-expand-lg  m-0">
+            <nav class="navbar navbar-expand-lg  m-0">
 
-            <a href="./"
-               class="navbar-brand">
-                <h1 class="text-start m-0">
-                    Aditya
-                </h1>
-            </a>
+                <a href="./"
+                   class="navbar-brand">
+                    <h1 class="text-start m-0">
+                        Aditya
+                    </h1>
+                </a>
 
-            <button class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#toggleMobileMenu"
-                    aria-controls="toggleMobileMenu"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
+                <button class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#toggleMobileMenu"
+                        aria-controls="toggleMobileMenu"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
 
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse "
-                 id="toggleMobileMenu">
-                <ul class="navbar-nav ms-auto">
-                    <li class="ms-3">
+                <div class="collapse navbar-collapse "
+                     id="toggleMobileMenu">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="ms-3">
+                            <a class="nav-link"
+                               href="./"> Home </a>
+                        </li>
+                        <li class="ms-3">
+                            <a class="nav-link"
+                               href="blog.html"> Blog </a>
+                        </li>
+                        <li class="ms-3">
                         <a class="nav-link"
-                           href="./"> Home </a>
-                    </li>
-                    <li class="ms-3">
+                        href="movies.html"> Movies </a>
+                        </li>
+                        <li class="ms-3">
                         <a class="nav-link"
-                           href="blog.html"> Blog </a>
-                    </li>
-                    <li class="ms-3">
-                    <a class="nav-link"
-                    href="movies.html"> Movies </a>
-                    </li>
-                    <li class="ms-3">
-                    <a class="nav-link"
-                    href="paintings.html"> Paintings </a>
-                    </li>
-                    <li class="ms-3">
-                    <a class="nav-link"
-                    href="eggshells.html"> Eggshells </a>
-                    </li>
-                    <li class="ms-3">
-                    <a class="nav-link"
-                    href="literature.html"> Literature </a>
-                    </li>
-                    <li class="ms-3">
+                        href="paintings.html"> Paintings </a>
+                        </li>
+                        <li class="ms-3">
                         <a class="nav-link"
-                           href="3d-models.html"> 3D Models </a>
-                    </li>
-                    <li class="ms-3">
+                        href="eggshells.html"> Eggshells </a>
+                        </li>
+                        <li class="ms-3">
                         <a class="nav-link"
-                           href="photography.html"> Photography </a>
-                    </li>
-                </ul>
+                        href="literature.html"> Literature </a>
+                        </li>
+                        <li class="ms-3">
+                            <a class="nav-link"
+                               href="3d-models.html"> 3D Models </a>
+                        </li>
+                        <li class="ms-3">
+                            <a class="nav-link"
+                               href="photography.html"> Photography </a>
+                        </li>
+                    </ul>
 
-            </div>
+                </div>
 
-        </nav>
+            </nav>
 
         <hr class="mt-0 mb-3">
+        
     </div>
         `
     }

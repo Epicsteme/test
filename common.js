6b1e7dd2c0b4a7653edfdb8c7d198bf8e7724069
Epicsteme <43105196+Epicsteme@users.noreply.github.com>
2025-08-15 22:14:51 +0530

    Update common.js

--- common.js
+++ common.js
@@ -129,10 +129,10 @@ window.addEventListener('load', () => {
     // Fade in hometitles after 1.0 seconds (1000 ms) 
     setTimeout(() => {
         document.getElementById('hometitles').classList.add('visible');
-    }, 1000);
+    }, 1500);
 
     // Fade in homelinks after 1.5 seconds (1500 ms) 
     setTimeout(() => {
         document.getElementById('homelinks').classList.add('visible');
-    }, 1500);
+    }, 3000);
 });

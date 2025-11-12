c6be2177cfd592ad2d8f42db3b0cec4793a56738
Epicsteme <43105196+Epicsteme@users.noreply.github.com>
2025-11-12 20:19:58 +0530

    updated epicsteme to beyondoxa

--- common.js
+++ common.js
@@ -46,7 +46,7 @@ class MyHeader extends HTMLElement {
             <a href="./" class="navbar-brand" aria-current="page">
                 
                 <h1 class="text-start" aria-hidden="true">
-                    Epicsteme
+                    Beyondoxa
                 </h1>
                 
             </a>
